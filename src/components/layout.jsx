774cd2683949a9ae05f4bd14c8bd92102fda5634
JonathanGzzBen <jonathangzzben@gmail.com>
2021-03-28 13:06:04 -0600

    Modify navbar

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -9,34 +9,26 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
+import Link from "next/link";
 
 export default function Layout({ children }) {
   //   return <Container fluid>{children}</Container>;
   return (
     <Container fluid>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Link href="/" passHref>
+          <Navbar.Brand>Velocidad Relativa</Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Link href="/" passHref>
+              <Nav.Link>Home</Nav.Link>
+            </Link>
+            <Link href="/" passHref>
+              <Nav.Link>Link</Nav.Link>
+            </Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Navbar>
       <Container fluid className="pt-4">
